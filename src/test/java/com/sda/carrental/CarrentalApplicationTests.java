490d028e08bd74156a9f1f32d446800a6571e265
markiewi <pawel.markiewicz@webfleet.com>
2022-11-29 19:51:06 +0100

    Created environment.

---
+++ src/test/java/com/sda/carrental/CarrentalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sda.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class CarrentalApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
