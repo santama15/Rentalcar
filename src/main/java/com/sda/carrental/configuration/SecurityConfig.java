b45f76da1554aceac07440f0ca8a724fb2216a83
markiewi <pawel.markiewicz@webfleet.com>
2022-12-05 20:21:23 +0100

    FEATURE/Pulled from master and extend controller.

--- src/main/java/com/sda/carrental/configuration/SecurityConfig.java
+++ src/main/java/com/sda/carrental/configuration/SecurityConfig.java
@@ -1,8 +1,19 @@
 package com.sda.carrental.configuration;
 
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(@Qualifier("customUserDetailsService") UserDetailsService userDetailsService)
+    {
+        this.userDetailsService = userDetailsService;
+    }
 }
--- src/main/java/com/sda/carrental/web/mvc/WelcomeController.java
+++ src/main/java/com/sda/carrental/web/mvc/WelcomeController.java
@@ -1,5 +1,23 @@
 package com.sda.carrental.web.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sda.carrental.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/home")
 public class WelcomeController
 {
+    private final UserService userService;
+    @GetMapping
+    public String home(ModelMap map) {
+
+        return "home";}
 }
