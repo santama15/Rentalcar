ab8815d3a097bb6c91e56cbe5dbfbc56d4036d20
Sztywniak <107143750+Michal-Bistula@users.noreply.github.com>
2022-12-07 01:50:35 +0100

    ADD: CreateIndexForm, CreateLoginForm, DepartmentRepository, DepartmentService, MvcConfiguration, MODIFY: SecurityConfig, CustomUserDetail/Service, Department, User, index.html, login.html,

--- src/main/java/com/sda/carrental/configuration/SecurityConfig.java
+++ src/main/java/com/sda/carrental/configuration/SecurityConfig.java
@@ -1,8 +1,58 @@
 package com.sda.carrental.configuration;
 
 
+import com.sda.carrental.model.users.User;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(@Qualifier("customUserDetailsService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers(HttpMethod.DELETE).hasAuthority(User.Roles.ROLE_ADMIN.name())
+
+                .antMatchers("/**").permitAll()   //** to znaczy, że wszystkie adresy powyżej '/', + puszcza ruch dla niezalogowanych uzytowników
+//                .antMatchers("/login").authenticated()
+
+                .anyRequest().permitAll()  //każdy inny request niezadekretowanie będzie dostępny dla niezalogowanego
+//                  .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .and()
+                .httpBasic()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/");
+//                .and()
+//                .csrf().disable();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {  //zdefiniowany globalnie i nie musimy przy każdym haśle dodawać {bcrypt} bcryptencodera
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
 }
--- src/main/java/com/sda/carrental/web/mvc/WelcomeController.java
+++ src/main/java/com/sda/carrental/web/mvc/WelcomeController.java
@@ -1,5 +1,26 @@
 package com.sda.carrental.web.mvc;
 
-public class WelcomeController
-{
+import com.sda.carrental.model.property.Department;
+import com.sda.carrental.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class WelcomeController {
+
+
+    private final DepartmentService departmentService;
+    @GetMapping
+    public String welcomePage(final ModelMap map) {
+        List<Department> department = departmentService.findAll();
+        map.addAttribute("department", department);
+        return "index";
+    }
 }
