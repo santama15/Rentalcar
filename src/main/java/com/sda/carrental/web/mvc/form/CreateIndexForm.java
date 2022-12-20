b04454b083f6b21e457307519a4e4a73b2cd4bb5
Sztywniak <107143750+Michal-Bistula@users.noreply.github.com>
2022-12-20 20:33:52 +0100

    fix: showResults.html i th

--- src/main/java/com/sda/carrental/web/mvc/form/CreateIndexForm.java
+++ src/main/java/com/sda/carrental/web/mvc/form/CreateIndexForm.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,11 +16,17 @@ public class CreateIndexForm {
 
     private Long branch_id; //dodane przeze mnie dla /index.html
 
-//    @NotBlank(message = "Pole nie może być puste")
+    @NotBlank(message = "Pole nie może być puste")
     private String city;
 
-//    @NotBlank(message = "Pole nie może być puste")
+    @NotBlank(message = "Pole nie może być puste")
     private String address;
+
+    @NotBlank(message = "Pole nie może być puste")
+    private LocalDate dateTo;
+
+    @NotBlank(message = "Pole nie może być puste")
+    private LocalDate dateCreated;
 
 
 }
