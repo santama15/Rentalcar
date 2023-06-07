1dbe5804d9f1b5e06a859a396ee0b3fca92eca01
Lavcrd <zlatkov.panayotov@gmail.com>
2023-06-07 22:20:46 +0200

    feature/html buttons to reservation management update/verification to reservation management update/added reserv records to PredefiniedData update/changed stylesheet href fix/fixed html buttons paddings delete/removed unnecessary html document

--- src/main/java/com/sda/carrental/service/VerificationService.java
+++ src/main/java/com/sda/carrental/service/VerificationService.java
@@ -2,23 +2,18 @@ package com.sda.carrental.service;
 
 import com.sda.carrental.exceptions.ResourceNotFoundException;
 import com.sda.carrental.model.users.Customer;
+import com.sda.carrental.model.users.Verification;
 import com.sda.carrental.repository.VerificationRepository;
-import com.sda.carrental.service.auth.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class VerificationService {
     private final VerificationRepository repository;
-    private final UserService userService;
-
-    public boolean isVerified(CustomUserDetails cud) {
-        return repository
-                .findByCustomer((Customer) userService.findByUsername(cud.getUsername()))
-                .isPresent();
-    }
 
     public HttpStatus deleteByCustomerId(Customer customer) {
         try {
@@ -30,5 +25,13 @@ public class VerificationService {
             err.printStackTrace();
             return HttpStatus.INTERNAL_SERVER_ERROR;
         }
+    }
+
+    public Optional<Verification> getOptionalVerificationByCustomer(Customer customer) {
+        return repository.findByCustomer(customer);
+    }
+
+    public Verification maskVerification(Verification verification) {
+        return new Verification(verification.getCustomer(), verification.getPersonalId().replaceAll("^...", "XXX"), verification.getDriverId().replaceAll("...$", "XXX"));
     }
 }
